efa541d4f70ef9147cfbbe4a29e1b91f278b5b71
Hanbi-Kim <74577775+hanbikim@users.noreply.github.com>
2021-11-05 13:44:26 +0900

    Add files via upload

---
+++ DevJournal/src/main/java/com/opendev3/devjournal/SingupLoginApplication.java
@@ -0,0 +1,13 @@
+package com.opendev3.devjournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingupLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingupLoginApplication.class, args);
+    }
+
+}
