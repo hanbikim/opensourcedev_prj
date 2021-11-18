40d2df165a50d669152eb6a6266753c19a203223
Sooyeon Yim <yimsooyeon9@g.cbnu.ac.kr>
2021-11-18 23:08:20 +0900

    First commit

---
+++ src/test/java/com/example/DevJournal_2021_ModifiedTemplates/DevJournal2021ModifiedTemplatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevJournal_2021_ModifiedTemplates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevJournal2021ModifiedTemplatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
