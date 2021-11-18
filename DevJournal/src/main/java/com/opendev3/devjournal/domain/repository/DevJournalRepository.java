83a9c173535bf4a08dcba85460d1d5a22ca0c6bc
sooyeonyim-t <yimsooyeon9@g.cbnu.ac.kr>
2021-11-19 02:13:51 +0900

    Revert "Merge pull request #1 from hanbikim/templates"
    This reverts commit e44762e5b8bd5af0ca61ed72200299585b3fde5e, reversing
    changes made to 1bd6bcf076707134824a1ea9efc6973725874fd3.

--- DevJournal/src/main/java/com/opendev3/devjournal/domain/repository/DevJournalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.opendev3.devjournal.domain.repository;
-
-import com.opendev3.devjournal.domain.entity.DevJournalEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DevJournalRepository extends JpaRepository<DevJournalEntity, Long>{
-}
