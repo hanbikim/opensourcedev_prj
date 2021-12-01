9d4ce67652ee29f6773728d2f9a11d21461e73cd
chahyeonnaa <gusdk21650@naver.com>
2021-12-01 14:54:08 +0900

    searching-paging-meeting

--- src/main/java/com/opendev3/devjournal/domain/repository/MeetingRepository.java
+++ src/main/java/com/opendev3/devjournal/domain/repository/MeetingRepository.java
@@ -1,7 +1,13 @@
 package com.opendev3.devjournal.domain.repository;
 
+import com.opendev3.devjournal.domain.entity.DevJournalEntity;
 import com.opendev3.devjournal.domain.entity.MeetingEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MeetingRepository extends JpaRepository<MeetingEntity, Long> {
+    static List<MeetingEntity> findByTitleContaining(String keyword) {
+        return null;
+    }
 }
--- src/main/java/com/opendev3/devjournal/service/MeetingService.java
+++ src/main/java/com/opendev3/devjournal/service/MeetingService.java
@@ -7,6 +7,9 @@ import com.opendev3.devjournal.dto.DevJournalDto;
 import com.opendev3.devjournal.dto.MeetingDto;
 import com.opendev3.devjournal.domain.repository.MeetingRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -19,9 +22,13 @@ import java.util.Optional;
 public class MeetingService {
     private MeetingRepository meetingRepository;
 
+    private static final int BLOCK_PAGE_NUM_COUNT = 5;  // 블럭에 존재하는 페이지 번호 수
+    private static final int PAGE_POST_COUNT = 4;
+
     @Transactional
-    public List<MeetingDto> getMeetinglist() {
-        List<MeetingEntity> meetingEntities = meetingRepository.findAll();
+    public List<MeetingDto> getMeetinglist(Integer pageNum) {
+        Page<MeetingEntity> page = meetingRepository.findAll(PageRequest.of(pageNum - 1, PAGE_POST_COUNT, Sort.by(Sort.Direction.ASC, "createdDate")));
+        List<MeetingEntity> meetingEntities = page.getContent();
         List<MeetingDto> meetingDtoList = new ArrayList<>();
 
         for (MeetingEntity meetingEntity : meetingEntities) {
@@ -65,6 +72,11 @@ public class MeetingService {
     }
 
     @Transactional
+    public Long getBoardCount() {
+        return meetingRepository.count();
+    }
+
+    @Transactional
     public Long savePost(MeetingDto meetingDto) {
         return meetingRepository.save(meetingDto.toEntity()).getMeetid();
     }
@@ -73,6 +85,59 @@ public class MeetingService {
     public void deletePost(Long meetid) {
 
         meetingRepository.deleteById(meetid);
+    }
+
+    @Transactional
+    public List<MeetingDto> searchPosts(String keyword) {
+        List<MeetingEntity> meetingEntities = MeetingRepository.findByTitleContaining(keyword);
+        List<MeetingDto> meetingDtoList = new ArrayList<>();
+
+        if (meetingEntities.isEmpty()) return meetingDtoList;
+
+        for (MeetingEntity meetingEntity : meetingEntities) {
+            meetingDtoList.add(this.convertEntityToDto(meetingEntity));
+        }
+
+        return meetingDtoList;
+    }
+
+    public Integer[] getPageList(Integer curPageNum) {
+        Integer[] pageList = new Integer[BLOCK_PAGE_NUM_COUNT];
+
+        // 총 게시글 갯수
+        Double postsTotalCount = Double.valueOf(this.getBoardCount());
+
+        // 총 게시글 기준으로 계산한 마지막 페이지 번호 계산
+        Integer totalLastPageNum = (int)(Math.ceil((postsTotalCount/PAGE_POST_COUNT)));
+
+        // 현재 페이지를 기준으로 블럭의 마지막 페이지 번호 계산
+        Integer blockLastPageNum = (totalLastPageNum > curPageNum + BLOCK_PAGE_NUM_COUNT)
+                ? curPageNum + BLOCK_PAGE_NUM_COUNT
+                : totalLastPageNum;
+
+        // 페이지 시작 번호 조정
+        curPageNum = (curPageNum <= 3) ? 1 : curPageNum - 2;
+
+        // 페이지 번호 할당
+        for (int val = curPageNum, idx = 0; val <= blockLastPageNum; val++, idx++) {
+            pageList[idx] = val;
+        }
+
+        return pageList;
+    }
+
+    private MeetingDto convertEntityToDto(MeetingEntity meetingEntity) {
+        return MeetingDto.builder()
+                .meetid(meetingEntity.getMeetid())
+                .mtitle(meetingEntity.getMtitle())
+                .place(meetingEntity.getPlace())
+                .people(meetingEntity.getPeople())
+                .purpose(meetingEntity.getPurpose())
+                .pre(meetingEntity.getPre())
+                .mcontent(meetingEntity.getMcontent())
+                .post(meetingEntity.getPost())
+                .createdDate(meetingEntity.getCreatedDate())
+                .build();
     }
 
 
