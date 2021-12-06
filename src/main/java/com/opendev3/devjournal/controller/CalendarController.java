35caf94275745fb4da994c32adf129baa938b088
Kimseona34 <90442115+Kimseona34@users.noreply.github.com>
2021-12-06 09:50:29 +0900

    Update CalendarController.java

--- src/main/java/com/opendev3/devjournal/controller/CalendarController.java
+++ src/main/java/com/opendev3/devjournal/controller/CalendarController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CalendarController {
     @GetMapping("/calendar")
     public String calendar() {
-        return "Calendar/index";
+        return "Calendar/calendar";
     }
 }
